8e7e3cb72781f1b4cbc03e2222c8006171ea1d56
eminqasimov <eminqasimov@outlook.com>
2021-10-09 20:29:48 +0400

    Filter, Sort, Page, Search features add

--- src/structure/index.tsx
+++ src/structure/index.tsx
@@ -1,6 +1,18 @@
-import { TreeList, Editing, Column, Lookup } from "devextreme-react/tree-list"
+import {
+  TreeList,
+  Editing,
+  Column,
+  Lookup,
+  Scrolling,
+  Paging,
+  Pager,
+  SearchPanel,
+  HeaderFilter,
+} from "devextreme-react/tree-list"
 import { Template } from "devextreme-react/core/template"
 import { employees } from "./data"
+
+const allowedPageSizes = [5, 10, 20]
 
 function StatusCell(options) {
   return options.data?.status ? "Active" : "Deactive"
@@ -18,6 +30,16 @@ export default function Structure() {
         keyExpr="id"
         parentIdExpr="parent_id"
       >
+        <Scrolling mode="standard" />
+        <Paging enabled={true} defaultPageSize={5} />
+        <Pager
+          showPageSizeSelector={true}
+          allowedPageSizes={allowedPageSizes}
+          showInfo={true}
+        />
+        <SearchPanel visible={true} width={250} />
+        <HeaderFilter visible={true} />
+
         <Editing
           allowUpdating={true}
           allowDeleting={true}
