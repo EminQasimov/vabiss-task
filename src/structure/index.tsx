f9f9ea46e180b86aba31184f15079f0329b6a486
eminqasimov <eminqasimov@outlook.com>
2021-10-09 20:39:58 +0400

    State persisted by using Devextreme localStore

--- src/structure/index.tsx
+++ src/structure/index.tsx
@@ -8,22 +8,40 @@ import {
   Pager,
   SearchPanel,
   HeaderFilter,
+  StateStoring,
 } from "devextreme-react/tree-list"
 import { Template } from "devextreme-react/core/template"
 import { employees } from "./data"
+import { dataSource } from "./store"
 
 const allowedPageSizes = [5, 10, 20]
 
-function StatusCell(options) {
+function StatusCell(options: any) {
   return options.data?.status ? "Active" : "Deactive"
 }
 
 export default function Structure() {
+  function populateFakeData() {
+    employees.forEach((item) => {
+      dataSource.store().insert({ ...item })
+    })
+    dataSource.reload()
+  }
+
+  function resetLocalStorage() {
+    localStorage.clear()
+    dataSource.store().clear()
+    dataSource.reload()
+  }
+
   return (
     <div id="tree-list-demo">
+      <button onClick={populateFakeData}>Populate fake data</button>
+      <button onClick={resetLocalStorage}>Reset all state and storage</button>
+
       <TreeList
         id="employees"
-        dataSource={employees}
+        dataSource={dataSource}
         showRowLines={true}
         showBorders={true}
         columnAutoWidth={true}
@@ -40,6 +58,12 @@ export default function Structure() {
         <SearchPanel visible={true} width={250} />
         <HeaderFilter visible={true} />
 
+        <StateStoring
+          enabled={true}
+          type="localStorage"
+          storageKey="treeListState"
+        />
+
         <Editing
           allowUpdating={true}
           allowDeleting={true}
@@ -52,7 +76,7 @@ export default function Structure() {
         <Column dataField="parent_id" caption="Structure">
           <Lookup
             dataSource={{
-              store: employees,
+              store: dataSource.store(),
             }}
             valueExpr="id"
             displayExpr="name"
